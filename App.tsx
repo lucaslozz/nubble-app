41fecc0e8ea5dd631fc3691f5df0039bb8fa1864
Lucas Loz <lucass.mark@gmail.com>
2024-01-20 10:54:59 -0300

    build: install react query

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {ToastProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
@@ -9,13 +10,17 @@ import {Toast} from '@components';
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
         <ToastProvider>
-          <Router />
-          <Toast />
+          <QueryClientProvider client={queryClient}>
+            <Router />
+            <Toast />
+          </QueryClientProvider>
         </ToastProvider>
       </ThemeProvider>
     </SafeAreaProvider>
