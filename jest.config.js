2a51318eb68d4f9b20bcbabbaf3b2727c5f1a33a
Lucas Loz <lucas.loz@cebraspe.org.br>
2024-02-12 10:42:39 -0300

    test: create custom render

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleDirectories: ['node_modules', './src/test'],
 };
--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {render} from '@testing-library/react-native';
+import {render} from 'test-utils';
 
 import {Button} from '../Button';
 
